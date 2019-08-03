7124a3be6da5e3b5903a08420e37133515bed9ff
Ji Zhi Qian <jizhi.qian@jimubox.com>
2019-08-03 17:50:41 +0800

    Mock 接口： 登录，注销，浏览

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# iJmeter
+# 压力测试项目，练习用
+包含： JMeter压测脚本，模拟被测试的接口应用
+
+演示知识点:
+- Get请求编写
+- Get请求的传参
+- 获取返回值
+- Post请求编写
+- Post请求传参数
+- 获取返回值
+- JSON解析
+- 正则匹配
+- 动态参数传递
+- 公共参数配置
+- CSV导入参数配置
+- BeanShell初探
+
+模拟接口服务的启动方法
+python3 <path>/orderservice.py
---
+++ application/orderservice.py
@@ -0,0 +1,217 @@
+import json
+
+from flask import Flask, request, make_response, jsonify
+
+app = Flask(__name__)
+user_token_dic = {
+    'user01': '3b6754f00bb0063071c5b71ce2b56b4ed0ce56a63493e785bea85b74c41ce200'
+    , 'user02': 'dcdc7286f6822dc5a4b54d2ce53f40ae0e562d7fc06d64b8dd332f7d528c7c67'
+    , 'user03': '04c4fba92b3451b16daedbbc7dd4cec84b8a88333f3da60626c6abbfdc829a02'
+    , 'user04': '3c3a7fa64c34e2530b708ec9bdeedd49e0098bfd3239b1cd58985feeb38531b8'
+    , 'user05': 'abb9d47918055902a8a6f3f8e86cd962d37d49647e9b51bdd7f7d67ff95f9864'
+    , 'user06': '6b2b8f34dd4ad1812a76a7037ed23d9ebc0189070320fa32dc610a79852419c7'
+    , 'user07': 'aac287a1eeb39d1983aaa732a7a5f33a64918adb1e88da6b344d2090eaed7239'
+    , 'user08': '3d0c7ff2ad3063bc0e93befeef895def90290114ee590ca09b60f8e4476aca27'
+    , 'user09': 'a0a9941e29cebdd732a2a57411f14d45e14bc3061d66c2e1c30eca421ed0b02d'
+    , 'user10': '34467677a88fc7966807c26ced0972cc775be9e6983d4bfbd2a0981a4350dcfd'
+}
+menu_json = {
+    "breakfast": [
+        {
+            "menu_nunber": "01",
+            "menu_price": 5.50,
+            "menu_name": "小笼包"
+        },
+        {
+            "menu_nunber": "02",
+            "menu_price": 3.00,
+            "menu_name": "八宝粥"
+        },
+        {
+            "menu_nunber": "03",
+            "menu_price": 1.50,
+            "menu_name": "油条"
+        },
+        {
+            "menu_nunber": "04",
+            "menu_price": 1.00,
+            "menu_name": "茶叶蛋"
+        },
+        {
+            "menu_nunber": "05",
+            "menu_price": 1.50,
+            "menu_name": "豆包"
+        },
+        {
+            "menu_nunber": "06",
+            "menu_price": 2.00,
+            "menu_name": "烧饼"
+        }
+    ],
+    "lunch": [
+        {
+            "menu_nunber": "07",
+            "menu_price": 17.00,
+            "menu_name": "宫保鸡丁"
+        },
+        {
+            "menu_nunber": "08",
+            "menu_price": 25.00,
+            "menu_name": "锅包肉"
+        },
+        {
+            "menu_nunber": "09",
+            "menu_price": 35.00,
+            "menu_name": "糖醋排骨"
+        },
+        {
+            "menu_nunber": "10",
+            "menu_price": 30.00,
+            "menu_name": "蒜台炒肉片"
+        },
+        {
+            "menu_nunber": "11",
+            "menu_price": 25.50,
+            "menu_name": "酸辣土豆丝"
+        },
+        {
+            "menu_nunber": "12",
+            "menu_price": 32.00,
+            "menu_name": "红烧茄子"
+        }
+    ],
+    "dinner": [
+        {
+            "menu_nunber": "13",
+            "menu_price": 21.00,
+            "menu_name": "辣椒炒肉"
+        },
+        {
+            "menu_nunber": "14",
+            "menu_price": 15.00,
+            "menu_name": "豌豆肉片汤"
+        },
+        {
+            "menu_nunber": "15",
+            "menu_price": 26.00,
+            "menu_name": "原味五花肉卷"
+        },
+        {
+            "menu_nunber": "16",
+            "menu_price": 20.00,
+            "menu_name": "醋溜白菜"
+        },
+        {
+            "menu_nunber": "17",
+            "menu_price": 25.50,
+            "menu_name": "地三鲜"
+        },
+        {
+            "menu_nunber": "18",
+            "menu_price": 39.00,
+            "menu_name": "红烧排骨"
+        }
+    ]
+}
+
+
+@app.route("/api/v1/user/login", methods=['POST'])
+def login():
+    """登录接口，输入参数格式：
+        "authRequest": {
+            "userName": "{{login_email}}",
+            "password": "{{login_pwd}}"
+        }
+
+        :return
+        Success : http code 200
+        {
+            "code": "AUTH200",
+            "message": "login success",
+            "access_token": "3b6754f00bb0063071c5b71ce2b56b4ed0ce56a63493e785bea85b74c41ce200"
+        }
+
+        Fail : http code 401
+        {
+            "code": "AUTH401",
+            "message": "login fail"
+        }
+    """
+    raw_data = request.get_data(as_text=True)
+    data = json.loads(raw_data)
+    username = data.get("authRequest").get("userName")
+    password = data.get("authRequest").get("password")
+
+    if username not in user_token_dic.keys() or password != 'pwd':
+        print('Username or password error!')
+        login_fail_resp_data = {
+            "code": "AUTH401",
+            "message": "login fail"
+        }
+        return make_response(jsonify(login_fail_resp_data), '401')
+
+    login_succ_resp_data = {
+        "code": "AUTH200",
+        "message": "login success",
+        "access_token": user_token_dic.get(username)
+    }
+
+    return make_response(jsonify(login_succ_resp_data), '200')
+
+
+@app.route('/api/v1/menu/list', methods=['GET'])
+def list():
+    """菜单浏览接口
+
+    :return: 菜单列表
+    """
+    access_token = request.headers.get("access_token")
+
+    if access_token is None:
+        login_fail_resp_data = {
+            "code": "AUTH401",
+            "message": "Please login first."
+        }
+        return make_response(jsonify(login_fail_resp_data), '401')
+
+    if access_token not in user_token_dic.values():
+        print('access_token error, please re-login.')
+        login_fail_resp_data = {
+            "code": "AUTH401",
+            "message": "Unknown user info, please re-login."
+        }
+        return make_response(jsonify(login_fail_resp_data), '401')
+
+    category = request.args.get("type")
+
+    if category is not None:
+        return make_response(jsonify(menu_json.get(category)), 200)
+
+    return make_response(jsonify(menu_json), 200)
+
+
+@app.route("/api/v1/user/logout", methods=['POST'])
+def logout():
+    """用户注销接口，输入参数格式：
+        header = {'access_token' : ''}
+    """
+    access_token = request.headers.get("access_token")
+
+    if access_token not in user_token_dic.values():
+        print('access_token error, logout failed.')
+        login_fail_resp_data = {
+            "code": "AUTH401",
+            "message": "Unknown user info, logout fail."
+        }
+        return make_response(jsonify(login_fail_resp_data), '401')
+
+    login_succ_resp_data = {
+        "code": "AUTH200",
+        "message": "logout success"
+    }
+
+    return make_response(jsonify(login_succ_resp_data), '200')
+
+
+if __name__ == "__main__":
+    app.run(port=9000, debug=True, host='0.0.0.0')
