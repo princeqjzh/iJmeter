a399f4a73aee110f1a40a34bb6c20bd29d422c3c
princeqjzh <11547299@qq.com>
2023-05-22 09:33:00 +0800

    Jenkins auto stress test

--- automation/auto_stress_test_jenkins.sh
+++ automation/auto_stress_test_jenkins.sh
@@ -1,15 +1,20 @@
 #!/usr/bin/env bash
 
-# 压测脚本中设定的压测时间应为20秒
-export jmx_filename="order_auto.jmx"
+# 压测脚本中设定的压测时间从Jenkins任务参数中传入，参数名称 duration
+# 生成的压测报告入口文件为 ./index.html
+export jmx_filename="order_auto_jenkins.jmx"
 
 # 需要在系统变量中定义jmeter根目录的位置，如下
 # export jmeter_path="/your jmeter path/"
 
 echo "自动化压测开始"
 
+rm -f index.html
+echo "" > index.html
+
+
 # 压测并发数列表
-thread_number_array=(10 20 30)
+thread_number_array=($thread_number_list)
 for num in "${thread_number_array[@]}"
 do
     echo "压测并发数 ${num}"
@@ -21,8 +26,9 @@ do
     rm -rf ${web_report_path_name}
 
     # JMeter 静默压测 + 生成html压测报告
-    ${jmeter_path}/bin/jmeter -n -t ${jmx_filename} -l ${jtl_filename}  -Jthread=${num} -e -o ${web_report_path_name}
+    ${jmeter_path}/bin/jmeter -n -t ${jmx_filename} -l ${jtl_filename}  -Jthread=${num} -Jduration=${duration} -e -o ${web_report_path_name}
     echo "结束压测并发数 ${num}"
+    echo "<a href='${web_report_path_name}'>${web_report_path_name}</a><br><br>" >> index.html
 done
 echo "自动化压测全部结束"
 
